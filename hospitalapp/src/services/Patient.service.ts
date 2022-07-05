fcfa183ce1364ddaed34edd1876e3a252f8f3ee0
azgmohammadd <azgmohammadd@gmail.com>
2022-07-05 11:17:46 +0430

    use axios for fetch data

--- hospitalapp/src/services/Patient.service.ts
+++ hospitalapp/src/services/Patient.service.ts
@@ -5,13 +5,19 @@ import IPatientModel from "../models/IPatient.model";
 
 
 class PatientSerivce implements IPatientService{
-    
-    async GetAllPatients(skip: number, take: number): Promise<IPatientModel[]| null> {
-        return await fetch(`${patient_url}/${skip}/${take}`, {method: 'GET'})
-        .then(res => res.json())
-        .then(response => response)
-        .catch(error => console.log(error));
+    async GetAllPatients(skip: number, take: number): Promise<IPatientModel[] | null> {
+        return await axios.get(`${patient_url}/${skip}/${take}`)
+        .then((response) => response.data)
+        .catch((error) => console.error(error));
     }
+    
+    // async GetAllPatients(skip: number, take: number): Promise<IPatientModel[]| null> {
+    //     return await fetch(`${patient_url}/${skip}/${take}`, {method: 'GET', headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}})
+    //     .then(res => res.json())
+    //     .then(response => response)
+    //     .catch(error => console.log(error));
+    // }
+
     async GetPatientById(id: number): Promise<IPatientModel> {
         return await axios.get(`${patient_url}/${id}`).then(res => res.data).catch(error => console.log(error));
     }
