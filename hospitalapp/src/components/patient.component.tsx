a1613eeeceeab32179ae49051dc0c6c70eab3dbf
azgmohammadd <azgmohammadd@gmail.com>
2022-07-07 10:31:53 +0430

    update component:
    add data for update, and connect to redux for dispatch actions

--- hospitalapp/src/components/patient.component.tsx
+++ hospitalapp/src/components/patient.component.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 import IPatientService from '../interfaces/IPatient.service';
 import { AppDispatch } from '../redux/store';
 import PatientService from '../services/Patient.service';
-import { getPatients } from '../redux/actions/patient.actions';
-import { getAllAction } from '../redux/actions/patient.newactio';
+import { getAllPatients } from '../redux/actions/patient.actions';
+// import { getAllAction } from '../redux/actions/patient.newactio';
+import { getAction } from '../redux/actions/getAction';
+import { updateAction } from '../redux/actions/update.patient';
+import IPatientModel from '../models/IPatient.model';
 
 class PatientComponent extends React.Component<any, any> {
 
@@ -14,8 +17,22 @@ class PatientComponent extends React.Component<any, any> {
         super(props);
         this.state = {
             id: 1,
-            skip:1,
+            skip:0,
             take:3,
+            data: {
+                "_id": 1,
+                "firstName": "mohammad",
+                "lastName": "azg",
+                "email": "email2",
+                "password": "you can!",
+                "age": 2,
+                "gender": "gender2",
+                "city": "city2",
+                "state": "state2",
+                "disease": "disease2",
+                "phoneNumber": "0123456789",
+                "doctorID": "1"
+              },
         };
         this.patservice = new PatientService();
         this.getdata = this.getdata.bind(this);
@@ -33,7 +50,9 @@ class PatientComponent extends React.Component<any, any> {
                 patient.component
                 <button onClick={() => console.log(this.props.state)}> show map state</button>
                 <button onClick={this.getdata}> show data</button>
-                <button onClick={() => this.props.getAllPatients(this.state.skip, this.state.take)}> disptach action</button>
+                <button onClick={() => this.props.getAllPatients(this.state.skip, this.state.take)}> disptach get all action</button>
+                <button onClick={() => this.props.getPatient(this.state.id)}> disptach get  action</button>
+                <button onClick={() => this.props.updatePatient(this.state.data)}> disptach update  action</button>
             </div>
         )
     }
@@ -50,7 +69,11 @@ function mapStateToProps(state: any){
 function mapDispatchToProps(dispatch: AppDispatch){
     return {
         getAllPatients: function(skip: number, take: number){
-            dispatch(getAllAction(skip, take))
+            dispatch(getAllPatients(skip, take))
+        },getPatient: function(id: number){
+            dispatch(getAction(id))
+        },updatePatient: function(model : IPatientModel){
+            dispatch(updateAction(model))
         }
     }
 }
