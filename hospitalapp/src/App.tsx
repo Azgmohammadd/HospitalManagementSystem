6c6ebfc30ca43dd335408165d8ba921a1f720032
azgmohammadd <azgmohammadd@gmail.com>
2022-07-07 10:34:59 +0430

    add react-redux provider to app

--- hospitalapp/src/App.tsx
+++ hospitalapp/src/App.tsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import PatientComponent from "./components/patient.component";
-
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import TableCompoent from './components/table.component';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <PatientComponent />
-      </div>
+      <Provider store={store} >
+        <div>
+          {/* <PatientComponent /> */}
+          <TableCompoent />
+        </div>
+      </Provider>
     )
   }
 }
